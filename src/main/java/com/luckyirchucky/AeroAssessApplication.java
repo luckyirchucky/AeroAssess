94b3f3f33df61a3316fcaebde557b2eece75b096
irinakorshakova <i.korshakova@nicetu.spb.ru>
2024-04-18 00:59:19 +0300

    fix

--- src/main/java/com/luckyirchucky/AeroAssessApplication.java
+++ src/main/java/com/luckyirchucky/AeroAssessApplication.java
@@ -3,6 +3,7 @@ package com.luckyirchucky;
 import com.luckyirchucky.ui.main.MainWindow;
 
 public class AeroAssessApplication {
+
     public static void main(String[] args) {
         new MainWindow();
     }
